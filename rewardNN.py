17ba6a4c057f575221a80f2c1cbfbb1a4459899a
Yan Yang <dimply@126.com>
2019-04-29 06:32:35 +0000

    change action to one hot in rewardNN and add plot

--- rewardNN.py
+++ rewardNN.py
@@ -1,35 +1,43 @@
 import tensorflow as tf
 import numpy as np
-import json, gzip
+import json, gzip, sys
+import matplotlib.pyplot as plt
 
 with gzip.GzipFile('env-train.json', 'r') as fin:
     json_bytes = fin.read()
-
+print('file read')
 json_str = json_bytes.decode('utf-8')
-
+print('string decoded')
 data = json.loads(json_str)
+print('json parsed')
 total = len(data)
-print('file read', total)
+print(total)
 np.random.shuffle(data)
 print('shuffled')
 
-x = np.asarray([d['obs'] + [d['act']] for d in data])
+act_vectors = np.eye(88)
+x = []
+y = []
+for d in data:
+    x.append(d['obs'].extend(act_vectors[d['act']]))
+    y.append(d['r'])
+    print('.', end='')
+    sys.stdout.flush()
+x = np.asarray(x)
 mean = x.mean(axis=0)
 std = x.std(axis=0)
-print('waiting for last step...')
-y = np.asarray([d['r'] for d in data])
-
+y = np.asarray(y)
 
 print("data prepared")
 
-X = tf.placeholder(shape=(None, 26), dtype=tf.float32)
+X = tf.placeholder(shape=(None, 113), dtype=tf.float32)
 _X = tf.divide(tf.subtract(X, mean),std)
-H1 = tf.layers.dense(_X, 64, activation="relu")
-H2 = tf.layers.dense(H1, 64, activation="relu")
+H1 = tf.layers.dense(_X, 256, activation="relu")
+H2 = tf.layers.dense(H1, 128, activation="relu")
 Y = tf.layers.dense(H2, 1)
 
 L = tf.placeholder(shape=(None, ), dtype=tf.float32)
-loss = tf.reduce_sum(tf.square(tf.subtract(Y,L)))
+loss = tf.reduce_mean(tf.square(tf.subtract(Y,L)))
 rms = tf.train.RMSPropOptimizer(0.001)
 opt = rms.minimize(loss)
 
@@ -40,12 +48,17 @@ saver = tf.train.Saver()
 saver.save(sess, 'env_r_model', global_step=total)
 
 # batch training, one pass
+loss = []
 i = 0
 while i < total:
     end = min(i+128, total)
     loss_value, _ = sess.run([loss, opt], feed_dict={X:x[i:end], L:y[i:end]})
     i += 128
-    if i%200==0:
-        print("loss: " + loss_value.astype('str'))
+    if i%3200==0:
+        #print("loss: " + loss_value.astype('str'))
+        loss.append(loss_value)
+
+plt.plot(loss)
+plt.savefig('rewardLoss.png')
 
 print('training finished')
