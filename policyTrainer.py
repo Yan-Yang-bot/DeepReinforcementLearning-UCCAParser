21701c4db44918038294f26fd80dfac68dfc2b9e
Tianyu Yang <tianyuyang@Tianyus-MacBook-Pro.local>
2019-05-04 17:45:24 -0400

    saver added

--- policyTrainer.py
+++ policyTrainer.py
@@ -1,31 +1,30 @@
 import tensorflow as tf
-import tensorflow_probability as tfp
 import argparse
 import gym
 import sys
 import drl_ucca
 from glob import glob
-from tests.conftest import load_passage
+from passage2oracles import load_passage
 
 # Preparation for plotting
 import matplotlib.pyplot as plt
 ave_returns_plot = []
 
 # Other globals
-f_n = 0 # count the number of input files
+f_n = 0
 filenames = glob("data/raw/train-xml/*")
 batch_size = 40
+
 sess = tf.Session()
 train_data = []
 
-def generate_samples(env, debug):
+def generate_samples(env, args):
     """
     :param env: environment handler
-    :param debug: whether to switch on debug loggings
     """
     if 'state' not in globals():
         # Get params for action and state spaces
-        global train_data, sess, state, action_dist, f_n, filenames, saver
+        global train_data, state, action_dist, f_n, filename,saver
         n_state = 25
         n_action = 88
 
@@ -34,23 +33,20 @@ def generate_samples(env, debug):
         w1 = tf.get_variable("w1", shape=[n_state, 96])
         w2 = tf.get_variable("w2", shape=[96, 96])
         w3 = tf.get_variable("w3", shape=[96, n_action])
-        saver = tf.train.Saver({"w1":w1, "w2":w2, "w3":w3})
+        saver = tf.train.Saver({"w1":w1, "w2":w2,"w3":w3})
         o1 = tf.matmul(state, w1)
         h1 = tf.math.softmax(o1)
         o2 = tf.matmul(h1, w2)
         h2 = tf.math.softmax(o2)
         action_logits = tf.matmul(h2, w3)
         action_prob = tf.math.softmax(action_logits)
-        action_dist = tfp.distributions.Categorical(probs=action_prob[0])
+        action_dist = tf.distributions.Categorical(probs=action_prob[0])
 
         # Initialization & policy definition
-        sess.run(tf.global_variables_initializer())
-
-        if debug:
-            simpleActions = ['SHIFT', 'REDUCE', 'SWAP', 'FINISH']
-            allLabels = ['H', 'A', 'C', 'L', 'D', 'E', 'G', 'S', 'N', 'P', 'R', 'F', 'Terminal', 'U']
-            complexActions = ['IMPLICIT', 'NODE', 'RIGHT-EDGE', 'LEFT-EDGE', 'RIGHT-REMOTE', 'LEFT-REMOTE']
-
+        if args.restore ==0:
+            sess.run(tf.global_variables_initializer())
+        else:
+            saver.restore(sess, "policy_model/policymodel.ckpt")
 
     # Collect training data
     action_sample = action_dist.sample(sample_shape=())
@@ -58,9 +54,7 @@ def generate_samples(env, debug):
     train_data=[]
     print("Episode lengths: ")
     for _ in range(batch_size):
-        #TODO: Stop when all files are exhausted. Check f_n >= len(filenames), and consider:
-        #TODO:  1.  len(train_data) != batch_size
-        #TODO:  2.  Not only end episode, but also jump out of the n_iter loop in main immediately to plot part
+
         obs = env.reset(load_passage(filenames[f_n]))
         f_n += 1
         traj = []
@@ -68,13 +62,6 @@ def generate_samples(env, debug):
         while True:
             traj.append({'obs':obs})
             action = policy(obs)
-
-            if debug:
-                a_type = simpleActions[action] if action < 4 else complexActions[(action-4)//14]
-                label = None if action < 4 else allLabels[(action-4) % 14]
-                if a_type in ['SHIFT', 'REDUCE', 'NODE', 'SWAP', 'FINISH']:
-                    print(a_type+('-'+label if label else ''))
-
             traj[t]['act']=action
             obs, r, done, info = env.step(action)
             traj[t]['R']=r
@@ -108,10 +95,10 @@ def update_policy(alpha):
     """
     :param alpha: learning rate
     """
-    # Build dataflow for _g * log probabilities of sampled actions
+    # Build dataflow for G * log probabilities of sampled actions
     act = tf.placeholder(shape=(), dtype=tf.int32)
-    _g = tf.placeholder(shape=(), dtype=tf.float32)
-    obj = tf.multiply(_g, action_dist.log_prob(act))
+    G = tf.placeholder(shape=(), dtype=tf.float32)
+    obj = tf.multiply(G, action_dist.log_prob(act))
     # Optimizer pre-definition (minimizing negative object == maximizing object)
     neg_obj = tf.scalar_mul(-1,obj)
     adam = tf.train.AdamOptimizer(learning_rate=alpha)
@@ -126,7 +113,7 @@ def update_policy(alpha):
         for k in range(len(traj)):
             cur = traj[k]
             feed_dict={state: [cur['obs']],
-                       act: cur['act'],  _g: cur['R']}
+                       act: cur['act'],  G: cur['R']}
             obj_value, _ = sess.run([obj, opt], feed_dict=feed_dict)
             print("object:", obj_value)
         print('return: ', traj[0]['R'])
@@ -143,7 +130,7 @@ def main(args):
 
     # Iterations
     for _ in range(args.n_iter):
-        generate_samples(env, args.debug)
+        generate_samples(env, args)
         estimate_return(args.lmbd)
         update_policy(args.alpha)
 
@@ -157,13 +144,12 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', "--n_iter", type=int, default=20)
-    parser.add_argument('-b', "--lmbd", type=float, default=1.0)
-    parser.add_argument('-a', "--alpha", type=float, default=0.01)
-
-    parser.add_argument('-e', "--debug", action="store_true")
-
-    main(parser.parse_args())
-    saver.save(sess, "policy_model/policymodel.ckpt")
-
-sess.close()
+    parser.add_argument("--restore", type = int, default = 0)
+    parser.add_argument("--n_iter", type=int, default=20)
+    parser.add_argument("--lmbd", type=float, default=1.0)
+    parser.add_argument("--alpha", type=float, default=0.01)
+    
+    main(parser.parse_args()) 
+    if parser.parse_args().restore==0:
+        saver.save(sess, "policy_model/policymodel.ckpt")   
+    sess.close()
